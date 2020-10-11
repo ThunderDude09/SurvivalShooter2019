f5b95c3e8fa66a6434a4004c484a3fe97117d9ad
Thunder19 <braydenhamous@gmail.com>
2020-10-10 20:32:56 -0500

    third

---
+++ SurvivalShooter/SurvivalShooter/Assets/PlayerSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+
+    public GameObject player;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Spawn()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            Instantiate(player, transform.position, transform.rotation);
+        }
+        
+    }
+}
