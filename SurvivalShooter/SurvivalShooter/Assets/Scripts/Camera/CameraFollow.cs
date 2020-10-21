f519cf9f432a76eb555b19a03930a9ef26bcaff1
Thunder19 <braydenhamous@gmail.com>
2020-10-21 14:34:37 -0500

    1-8

--- SurvivalShooter/SurvivalShooter/Assets/Scripts/Camera/CameraFollow.cs
+++ SurvivalShooter/SurvivalShooter/Assets/Scripts/Camera/CameraFollow.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Mirror;
 
-public class CameraFollow : NetworkBehaviour
+
+public class CameraFollow : MonoBehaviour
 {
-	public Transform target;
+	//public float speed = 6f;
+	/*public Transform target;
 	public float smoothing = 5f;
 
 	private Vector3 offset;
@@ -17,7 +18,9 @@ public class CameraFollow : NetworkBehaviour
 
 	void FixedUpdate()
 	{
+		//transform.Translate(0, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+
 		Vector3 targetCamPos = target.position + offset;
 		transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
-	}
+	}*/
 }
