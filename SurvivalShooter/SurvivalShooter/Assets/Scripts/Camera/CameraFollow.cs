40b1d0a19be2f12f4b7cb2c94608df99c61ee379
Thunder19 <braydenhamous@gmail.com>
2020-10-21 14:09:44 -0500

    1-7

--- SurvivalShooter/SurvivalShooter/Assets/Scripts/Camera/CameraFollow.cs
+++ SurvivalShooter/SurvivalShooter/Assets/Scripts/Camera/CameraFollow.cs
@@ -5,26 +5,19 @@ using Mirror;
 
 public class CameraFollow : NetworkBehaviour
 {
-	/*public float speed = 6f;
-
 	public Transform target;
 	public float smoothing = 5f;
 
 	private Vector3 offset;
 
-    void Start()
+	void Start()
 	{
 		offset = transform.position - target.position;
 	}
 
 	void FixedUpdate()
 	{
-		if(Input.GetButtonDown("Horizontal"))
-		{
-			transform.Translate(Vector3.right * speed * Time.deltaTime);
-		}
-
 		Vector3 targetCamPos = target.position + offset;
 		transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
-	}*/
+	}
 }
